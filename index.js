5e4c22c822ca26ed84b700e07054452805ef0908
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-10-20 19:23:14 -0700

    Some problem solved

--- index.js
+++ index.js
@@ -31,81 +31,91 @@ async function run() {
     const productCollection = client.db('productDB').collection('product')
     const cartCollection = client.db('cartDB').collection('cart')
 
-    app.post('/brand', async(req, res) => {
-        const newBrand = req.body;
-        const result = await brandCollection.insertOne(newBrand)
-        res.send(result)
+    app.post('/brand', async (req, res) => {
+      const newBrand = req.body;
+      const result = await brandCollection.insertOne(newBrand)
+      res.send(result)
     })
 
-    app.post('/feedback', async(req, res) => {
+    app.post('/feedback', async (req, res) => {
       const newFeedback = req.body;
       const result = await feedbackCollection.insertOne(newFeedback)
       res.send(result)
     })
-    app.post('/member', async(req, res) => {
+    app.post('/member', async (req, res) => {
       const newMember = req.body;
       const result = await memberCollection.insertOne(newMember)
       res.send(result)
     })
-    app.post('/product', async(req, res) => {
+    app.post('/product', async (req, res) => {
       const newProduct = req.body;
-      console.log(newProduct);
       const result = await productCollection.insertOne(newProduct)
       res.send(result)
     })
-    app.post('/cart', async(req, res) => {
+    app.post('/cart', async (req, res) => {
       const newProduct = req.body;
-      console.log(newProduct);
       const result = await cartCollection.insertOne(newProduct)
       res.send(result)
     })
 
 
-    app.get('/feedback', async(req, res) => {
+    app.get('/feedback', async (req, res) => {
       const cursor = feedbackCollection.find();
       const result = await cursor.toArray();
       res.send(result)
-  })
-    app.get('/member', async(req, res) => {
+    })
+    app.get('/member', async (req, res) => {
       const cursor = memberCollection.find();
       const result = await cursor.toArray();
       res.send(result)
-  })
-
-    app.get('/brand', async(req, res) => {
-        const cursor = brandCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
-    })
-    app.get('/brand/:id', async(req, res) => {
-       const id = req.params.id;
-       const query = {_id: new ObjectId(id)}
-       const result = await brandCollection.findOne(query)
-       res.send(result)
-    })
-    app.get('/product', async(req, res) => {
-       const cursor = productCollection.find()
-       const result = await cursor.toArray()
-       console.log(result);
-       res.send(result)
-    })
-    app.get('/product/:id', async(req, res) => {
-       const id = req.params.id;
-       const query = {_id: new ObjectId(id)}
-       const result = await productCollection.findOne(query)
-       res.send(result)
     })
 
-    app.get('/cart', async(req, res) => {
+    app.get('/brand', async (req, res) => {
+      const cursor = brandCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    app.get('/brand/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await brandCollection.findOne(query)
+      res.send(result)
+    })
+    app.get('/product', async (req, res) => {
+      const cursor = productCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await productCollection.findOne(query)
+      res.send(result)
+    })
+    app.get('/cart', async (req, res) => {
       const cursor = cartCollection.find()
       const result = await cursor.toArray()
-      console.log(result);
       res.send(result)
-   })
+    })
 
-    /
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+
+    // app.get('/cart/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await cartCollection.findOne(query)
+    //   res.send(result)
+    // })
+
+    app.delete('/cart/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id }
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
+    })
+
+      /
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -116,9 +126,9 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send('Technology and electronics site is running')
+  res.send('Technology and electronics site is running')
 })
 
 app.listen(port, () => {
-    console.log(`Technology and electronics site is running port is: ${port}`);
+  console.log(`Technology and electronics site is running port is: ${port}`);
 })
